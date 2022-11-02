a14d6a52bed0ead4faf8d04df6127cdb5e18736b
fgomrom <fgomrom726@g.educaand.es>
2022-11-02 09:24:28 +0100

    Update EjemploSwitch.java
    Sentencia selectiva Switch

--- EjemplosUT2/EjemploSwitch.java
+++ EjemplosUT2/EjemploSwitch.java
@@ -1,24 +1,24 @@
 public class EjemploSwitch {
     public static void main(String[] args) {
 
-        int dia = 1;
+        int dia = 2;
 
         switch (dia) {
-            case 1: 
+            case 1:
                 System.out.println("Lunes");
                 break;
-            case 2: 
+            case 2:
                 System.out.println("Martes");
-                break;    
-            case 3: 
+                break;
+            case 3:
                 System.out.println("Miércoles");
-                break;    
-            case 4: 
+                break;
+            case 4:
                 System.out.println("Jueves");
-                break;    
-            case 5: 
+                break;
+            case 5:
                 System.out.println("Viernes");
-                break;    
+                break;
             default:
                 System.out.println("Fin de semana");
                 break;
