279f6a5ca9fe8d551c6f91fec34e6c0df860da9d
fgomrom <fgomrom726@g.educaand.es>
2022-11-03 13:22:03 +0100

    Random

--- EjemplosUT2/ParImparFromRandom.java
+++ EjemplosUT2/ParImparFromRandom.java
@@ -1,5 +1,6 @@
 public class ParImparFromRandom {
     public static void main(String[] args) {
-        
+        int numAleatoreo = (int)(Math.random()*100);
+        System.out.println(numAleatoreo);
     }
 }
