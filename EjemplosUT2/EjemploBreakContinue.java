54f20dbb0c09be68ee37229180b71ab6b68cad88
fgomrom <fgomrom726@g.educaand.es>
2022-11-04 14:56:16 +0100

    Break

---
+++ EjemplosUT2/EjemploBreakContinue.java
@@ -0,0 +1,12 @@
+public class EjemploBreakContinue {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5 ; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println("i: " + i);
+        }
+               
+    }
+}
