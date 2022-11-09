ba3b9cac1770e0524f34924dfb4972360760cd0d
fgomrom <fgomrom726@g.educaand.es>
2022-11-09 08:58:30 +0100

    Empieza en 1 y acaba en M

--- EjemplosUT2/TablaMultiplicar.java
+++ EjemplosUT2/TablaMultiplicar.java
@@ -1,8 +1,11 @@
 public class TablaMultiplicar {
+    private static final int M = 0;
+    private static final int N = 0;
+
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 1; i <= M; i++) {
             System.out.println("Tabla del " + i);
-            for (int j = 0; j <= 10; j++) {
+            for (int j = 1; j <= N; j++) {
                 System.out.println(i + " x " + j + " = " + i*j);
             }
         }
