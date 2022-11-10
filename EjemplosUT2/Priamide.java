b5a041e9b3f5f2bd01e2ef0b61d00f6b44dfe5b5
Fran Gomez <fgomrom726@g.educaand.es>
2022-11-10 20:39:36 +0100

    Piramide

---
+++ EjemplosUT2/Priamide.java
@@ -0,0 +1,16 @@
+public class Priamide {
+    private static final int NIVELES = 4;
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= NIVELES; i++) {
+            for (int j = 1; j <= NIVELES+i-1; j++) {
+                if (j <= NIVELES-i || j >= NIVELES+i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
