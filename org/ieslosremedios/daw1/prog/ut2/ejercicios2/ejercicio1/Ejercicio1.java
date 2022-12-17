7b8952665db46b5c3f368be878596961f0a03305
Fran Gómez <fgomrom726@g.educaand.es>
2022-12-17 11:49:45 +0100

    ejercicio 1

---
+++ org/ieslosremedios/daw1/prog/ut2/ejercicios2/ejercicio1/Ejercicio1.java
@@ -0,0 +1,8 @@
+package org.ieslosremedios.daw1.prog.ut2.ejercicios2.ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        char caracter = 'A';
+        System.out.println("\'" + caracter + "\' = " + (int)caracter);
+    }
+}
---
+++ org/ieslosremedios/daw1/prog/ut2/ejercicios2/ejercicio1/Ejercicio2.java
@@ -0,0 +1,8 @@
+package org.ieslosremedios.daw1.prog.ut2.ejercicios2.ejercicio1;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int notaMedia = 7;
+        System.out.printf("Nota media: %.3f", (float)notaMedia);
+    }
+}
