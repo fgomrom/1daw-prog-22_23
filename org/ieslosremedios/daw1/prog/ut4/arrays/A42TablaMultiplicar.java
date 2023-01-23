5d0da433d6be58eaaef32364d099b5e2aa4b0219
Fran Gómez <fgomrom726@g.educaand.es>
2023-01-23 13:56:48 +0100

    Tabla multiplicar con un sólo for

--- org/ieslosremedios/daw1/prog/ut4/arrays/A42TablaMultiplicar.java
+++ org/ieslosremedios/daw1/prog/ut4/arrays/A42TablaMultiplicar.java
@@ -1,30 +1,25 @@
 package org.ieslosremedios.daw1.prog.ut4.arrays;
 
-import java.util.Arrays;
-
 public class A42TablaMultiplicar {
     public static void main(String[] args) {
-        imprimeArray(creaTablaMultiplicar(1));
+        imprimeArray(creaTablaMultiplicar(2));
     }
 
     static int[][] creaTablaMultiplicar (int numero) {
         int [][] tabla = new int[10][3];
         for (int i = 0; i < tabla.length; i++) {
             tabla[i][0] = numero;
-            for (int j = 0; j < tabla[i].length - 1; j++) {
-                tabla[i][j + 1] = i + 1;
-            }
+            tabla[i][1] = i + 1;
+            tabla[i][2] = numero * tabla[i][1];
         }
         return tabla;
     }
 
     static void imprimeArray (int[][] a) {
         for (int i = 0; i < a.length; i++) {
-//            for (int j = 0; j < a[i].length; j++) {
-                System.out.print(a[i][0]);
-                System.out.print(a[i][1]);
-                System.out.print(a[i][2]);
-//            }
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j]);
+            }
             System.out.println();
         }
     }
