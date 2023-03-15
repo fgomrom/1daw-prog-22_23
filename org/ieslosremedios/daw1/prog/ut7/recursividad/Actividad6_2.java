f964b1fae725ede277e90d5bb414a475b24e9dd8
fgomrom <fgomrom726@g.educaand.es>
2023-03-15 10:31:45 +0100

    A6_2 Juanma S.

---
+++ org/ieslosremedios/daw1/prog/ut7/recursividad/Actividad6_2.java
@@ -0,0 +1,24 @@
+package org.ieslosremedios.daw1.prog.ut7.recursividad;
+
+public class Actividad6_2 {
+
+    public static void main(String[] args) {
+        inverso(5);
+    }
+
+    /*Ejercicio 1.
+    Crea una función que imprima los números naturales en orden inverso comenzando por el
+    número n.
+    Ejemplo:
+    Para n = 5 → 5, 4, 3, 2, 1
+    */
+    public static void inverso(int n) {
+        if (n > 0) {
+            // CASO RECURSIVO
+            System.out.println(n);
+            inverso(n - 1);
+        }
+        // CASO BASO
+    }
+
+}
