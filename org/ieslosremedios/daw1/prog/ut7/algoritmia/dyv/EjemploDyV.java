b66f18b196e884197f0ea13a47daaf3e800d21d5
Fran Gómez <fgomrom726@g.educaand.es>
2023-03-17 18:57:33 +0100

    Comparativa tiempos busqueda binaria y lineal.

--- org/ieslosremedios/daw1/prog/ut7/algoritmia/dyv/EjemploDyV.java
+++ org/ieslosremedios/daw1/prog/ut7/algoritmia/dyv/EjemploDyV.java
@@ -3,22 +3,47 @@ package org.ieslosremedios.daw1.prog.ut7.algoritmia.dyv;
 import java.util.Arrays;
 
 public class EjemploDyV {
+    private static final int N = 1000000;
+
     public static void main(String[] args) {
-        int[] a = {-2, -1, 3, 5, 8, 10};
+        //int[] a = {-2, -1, 3, 5, 8, 10};
+        int [] a = new int[N];
+        Arrays.fill(a,1);
         int num = 5;
+        long startTime = System.nanoTime();
         int pos = dyv(a, num);
-        System.out.println("El número " + num + " está en la posición: " + pos);
+        long endTime = System.nanoTime();
+        System.out.println("BinarySearch -> El número " + num + " está en la posición: " + pos);
+        System.out.println("BinarySearch -> Tiempo: " + (endTime - startTime) + "ns");
+        startTime = System.nanoTime();
+        pos = lineal(a, num);
+        endTime = System.nanoTime();
+        System.out.println("LinearSearch -> El número " + num + " está en la posición: " + pos);
+        System.out.println("LinearSearch -> Tiempo: " + (endTime - startTime) + "ns");
     }
 
     private static int dyv(int[] a, int num) {
         int pos = a.length / 2;
 
-        if (num == a[pos]) {
+        if (a.length > 0) {
+            return -1;
+        } else if (num == a[pos]) {
             return pos;
         } else if (num > a[pos]) {
             return pos + 1 + dyv(Arrays.copyOfRange(a, pos + 1, a.length), num);
         } else {
             return dyv(Arrays.copyOfRange(a, 0, pos), num);
         }
+    }
+
+    public static int lineal(int[] a, int num) {
+        int pos = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (num == a[i]) {
+                pos = i;
+                break;
+            }
+        }
+        return pos;
     }
 }
