e0df5e7b9221aa351f352c65fdbef1da1a9d8f08
fgomrom <fgomrom726@g.educaand.es>
2023-04-12 10:06:46 +0200

    Ejemplos modos de acceso

--- org/ieslosremedios/daw1/prog/ut7/ficheros/EjemploFile.java
+++ org/ieslosremedios/daw1/prog/ut7/ficheros/EjemploFile.java
@@ -1,26 +1,52 @@
 package org.ieslosremedios.daw1.prog.ut7.ficheros;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class EjemploFile {
     public static void main(String[] args) {
-        // Crear una carpeta
-        File miCarpeta = new File("C:\\Users\\Fran Gómez\\Downloads\\Nueva carpeta");
-        System.out.println(miCarpeta.mkdir());
-
-        // Crear un fichero
-        File miFichero = new File("C:\\Users\\Fran Gómez\\Downloads\\Nueva carpeta\\Test.txt");
         try {
+            // Crear un fichero
+            File miFichero = new File("C:\\Users\\Fran Gómez\\Downloads\\Nueva carpeta\\Test.txt");
             miFichero.createNewFile();
+
+            // Crear una carpeta
+            File miCarpeta = new File("C:\\Users\\Fran Gómez\\Downloads\\Nueva carpeta");
+            System.out.println(miCarpeta.mkdir());
+
+            // Elimina un fichero
+            //miFichero.delete();
+
+            // Elimina una carpeta después de haber borrado su contenido
+            //miCarpeta.delete();
+
+            // Abrir fichero en modo texto, escritura y sobreescribiendo
+            FileWriter fw = new FileWriter(miFichero);
+            fw.close();
+
+            // Abrir fichero en modo texto, escritura y añadiendo al final
+            fw = new FileWriter(miFichero, true);
+            fw.write("Fran");
+            fw.close();
+
+            // TODO: Abrir fichero en modo binario, escritura y añadiendo al final
+            FileOutputStream fos = new FileOutputStream(miFichero, true);
+            fos.write(" Gómez".getBytes());
+            fos.close();
+
+            // TODO: Abrir fichero en modo texto, lectura
+            FileReader fr = new FileReader(miFichero);
+            int i;
+            while ((i=fr.read()) != -1) {
+                System.out.print((char) i);
+            }
+
+            // TODO: Abrir fichero en modo binario, lectura
+            FileInputStream fis = new FileInputStream(miFichero);
+            System.out.println(new String (fis.readAllBytes()));
+
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
-        // Elimina un fichero
-        miFichero.delete();
-
-        // Elimina una carpeta después de haber borrado su contenido
-        miCarpeta.delete();
     }
 }
