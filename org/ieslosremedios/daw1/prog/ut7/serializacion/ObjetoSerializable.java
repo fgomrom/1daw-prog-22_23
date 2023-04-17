9ad3d4c803578920bf372668a2ead7a6b77f69dd
fgomrom <fgomrom726@g.educaand.es>
2023-04-17 12:51:47 +0200

    Modifico el UID

--- org/ieslosremedios/daw1/prog/ut7/serializacion/ObjetoSerializable.java
+++ org/ieslosremedios/daw1/prog/ut7/serializacion/ObjetoSerializable.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class ObjetoSerializable implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -4797208967054263305L;
 
     int atributo1;
     static int atributo2;
