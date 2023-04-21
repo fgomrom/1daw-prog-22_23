4f33dfa6a3c9686a93ab018666f1f055204f72b8
Fran Gómez <fgomrom726@g.educaand.es>
2023-04-21 12:05:08 +0200

    Ejemplo lectura/escritura completo

--- org/ieslosremedios/daw1/prog/ut7/xml/EjemploEscrituraXML.java
+++ org/ieslosremedios/daw1/prog/ut7/xml/EjemploEscrituraXML.java
@@ -3,6 +3,7 @@ package org.ieslosremedios.daw1.prog.ut7.xml;
 import org.w3c.dom.Comment;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,36 +16,34 @@ import java.io.File;
 
 public class EjemploEscrituraXML {
     public static void main(String[] args) throws ParserConfigurationException, TransformerException {
-        // Document --> XML
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
-        Document document = factory.newDocumentBuilder().newDocument();
+        // Creamos el documento vacío para añadirle a continuación los nodos
+        // En este caso lo hago todo en una sola línea
+        Document document = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder().newDocument();
 
-        // create the root element node
-        Element element = document.createElement("root");
-        document.appendChild(element);
+        // Creamos el nodo raíz
+        Element estudiantes = document.createElement("estudiantes");
+        // Hacemos que cuelgue del documento (estructura de árbol)
+        document.appendChild(estudiantes);
 
-        // create a comment node given the specified string
-        Comment comment = document.createComment("This is a comment");
-        document.insertBefore(comment, element);
+        // Creamos el primer nodo y lo colgamos de su padre, el nodo raíz. --> <estudiante></estudiante>
+        Element estudianteFran = document.createElement("estudiante");
+        estudiantes.appendChild(estudianteFran);
 
-        // add element after the first child of the root element
-        Element itemElement = document.createElement("item");
-        element.appendChild(itemElement);
+        // Creamos un nodo de texto que será el valor del elemento anterior
+        Text fran = document.createTextNode("Fran");
+        // y lo colgamos del nodo anterior --> <estudiante>Fran</estudiante>
+        estudianteFran.appendChild(fran);
 
-        // add an attribute to the node
-        itemElement.setAttribute("myattr", "attrvalue");
+        // Igual para el siguiente esutidante
+        Element estudianteMenganito = document.createElement("estudiante");
+        estudiantes.appendChild(estudianteMenganito);
+        Text menganito = document.createTextNode("Menganito");
+        estudianteMenganito.appendChild(menganito);
 
-        // create text for the node
-        itemElement.insertBefore(document.createTextNode("text"), itemElement.getLastChild());
-
-        // Obtención del TransfomerFactory y del Transformer a partir de él.
+        // Clases necesarias para finalizar la creación del archivo XML
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
-
-        // Creación de la fuente XML a partir del documento.
         DOMSource source = new DOMSource(document);
-
-        // Creación del resultado, que será un fichero.
         StreamResult result = new StreamResult(new File("org/ieslosremedios/daw1/prog/ut7/xml/otro.xml"));
 
         // Se realiza la transformación, de Document a Fichero.
--- org/ieslosremedios/daw1/prog/ut7/xml/EjemploLecturaXML.java
+++ org/ieslosremedios/daw1/prog/ut7/xml/EjemploLecturaXML.java
@@ -15,21 +15,35 @@ import java.io.IOException;
 
 public class EjemploLecturaXML {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
-        Document document = builder.parse(new File("org/ieslosremedios/daw1/prog/ut7/xml/ejemplo.xml"));
 
-        System.out.println("Elemento raiz:" + document.getDocumentElement().getNodeName());
-        NodeList estudiantes = document.getElementsByTagName("estudiante");
-        for (int i = 0; i < estudiantes.getLength(); i++) {
-            Node nodeEstudiante = estudiantes.item(i);
-            if (nodeEstudiante.getNodeType() == Node.ELEMENT_NODE) {
-                Element elementEstudiante = (Element) nodeEstudiante;
-                NodeList nodosDeEstudiante = elementEstudiante.getChildNodes();
-                for (int j = 0; j < nodosDeEstudiante.getLength(); j++) {
-                    System.out.println(elementEstudiante.getTextContent());
+        // Cargamos fichero que vamos a leer
+        File file = new File("org/ieslosremedios/daw1/prog/ut7/xml/ejemplo.xml");
+
+        try {
+            // Parseamos el fichero al Document
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(file);
+
+            // Accedemos al nodo raíz
+            System.out.println("Elemento raiz: " + document.getDocumentElement().getNodeName());
+            System.out.println("Tipo de node: " + document.getDocumentElement().getNodeType());
+
+            // Accedemos a todos los nodos con el tag "estudiante"
+            NodeList estudiantes = document.getElementsByTagName("estudiante");
+            // Recorremos todos esos nodos
+            for (int i = 0; i < estudiantes.getLength(); i++) {
+                Node nodeEstudiante = estudiantes.item(i);
+                // Filtramos todos los que son nodos de tipo elemento
+                if (nodeEstudiante.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elementEstudiante = (Element) nodeEstudiante;
+                    System.out.println("Nombre del estudiante: " + elementEstudiante.getTextContent());
                 }
             }
+
+        } catch(Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
