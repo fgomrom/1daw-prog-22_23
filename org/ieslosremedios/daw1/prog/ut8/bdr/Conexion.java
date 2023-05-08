428f52add1fc8f3766c21b8e912cc070b5cde39d
fgomrom <fgomrom726@g.educaand.es>
2023-05-08 12:38:35 +0200

    Ejemplo conexión jdbc

---
+++ org/ieslosremedios/daw1/prog/ut8/bdr/Conexion.java
@@ -0,0 +1,25 @@
+package org.ieslosremedios.daw1.prog.ut8.bdr;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+    public static void main(String[] args) {
+        String url = "jdbc:mariadb://localhost:3306/tienda";
+        try {
+            // Enlazar con el driver
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(url, "root", "root");
+            Statement stm = con.createStatement();
+            //TODO consultas
+
+            stm.close();
+            con.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Ha fallado la conexión: " + e.getMessage());
+        }
+    }
+}
