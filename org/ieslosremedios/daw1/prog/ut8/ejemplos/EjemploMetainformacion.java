e62df1da2e98acd2a16e82694dbdd3f2eae11577
fgomrom <fgomrom726@g.educaand.es>
2023-05-18 15:01:32 +0200

    Ejercicio 12

--- org/ieslosremedios/daw1/prog/ut8/ejemplos/EjemploMetainformacion.java
+++ org/ieslosremedios/daw1/prog/ut8/ejemplos/EjemploMetainformacion.java
@@ -6,8 +6,20 @@ public class EjemploMetainformacion {
     public static void main(String[] args) {
         String url = "jdbc:mariadb://localhost:3306/tienda";
         try {
-            Connection con = DriverManager.getConnection(url, "root", "2107");
+            Connection con = DriverManager.getConnection(url, "root", "");
             Statement stm = con.createStatement();
+
+            boolean hayResultSet = stm.execute("SELECT nombre FROM producto WHERE LOWER(pais) like 'espana'");
+            ResultSet r = stm.executeQuery("SELECT nombre FROM producto WHERE LOWER(pais) like 'espana'");
+
+            if (hayResultSet) {
+                ResultSet rs = stm.getResultSet();
+                while (rs.next()) {
+                    System.out.println("- " + rs.getString("nombre"));
+                }
+            } else {
+                System.out.println("No existen productos de España.");
+            }
 
             // Alternativa con Statement
             /*String query = "SHOW DATABASES";
@@ -19,13 +31,28 @@ public class EjemploMetainformacion {
             }*/
 
             // Alternativa con DataaseMetaData
-            DatabaseMetaData metaData = con.getMetaData();
+            /*DatabaseMetaData metaData = con.getMetaData();
             ResultSet resultados = metaData.getCatalogs();
             System.out.println("Bases de datos existentes:");
             while (resultados.next()) {
                 System.out.println("- " + resultados.getString("TABLE_CAT"));
             }
-            System.out.println("Usuario: " + metaData.getUserName());
+            System.out.println("Usuario: " + metaData.getUserName());*/
+
+            // Tablas de la base de datos tienda
+            DatabaseMetaData metaData = con.getMetaData();
+            ResultSet resultados = metaData.getTables(null, "tienda", null, null);
+            System.out.println("Tablas existentes en tienda");
+            resultados = metaData.getTables("tienda", null, null, null);
+            while (resultados.next()) {
+                System.out.println("- " + resultados.getString("TABLE_NAME"));
+            }
+
+            // Tablas de la base de datos tienda
+            resultados = metaData.getColumns("tienda", null, "producto", null);
+            while (resultados.next()) {
+                System.out.println("- " + resultados.getString("COLUMN_NAME"));
+            }
 
             stm.close();
             con.close();
